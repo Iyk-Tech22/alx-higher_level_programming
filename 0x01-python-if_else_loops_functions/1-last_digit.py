ddd0288e2d03ccf352ceff6c6685464b6eb3bc55
Iyk <ikechukwugodwin22@yahoo.com>
2022-09-20 13:15:24 +0000

    Create last digit task 0

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-1000, 1000)
+last_dt = number % 10
+if last_dt > 5:
+    print(f"last digit of {number} is {last_dt} and is greater than 5")
+elif last_dt == 0:
+    print(f"last digit of {number} is {last_dt} and is 0")
+else:
+    print(f"last digit of {number} is {last_dt} and is less than 6 and not 0")
