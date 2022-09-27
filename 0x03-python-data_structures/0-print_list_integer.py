358ee596a3cf291bad02db26f3d33712dff35ef4
Iyk <ikechukwugodwin22@yahoo.com>
2022-09-27 02:13:10 +0000

    Update file 0-print_list_integer 1x

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,6 +3,7 @@
 
 # print all items in a list
 def print_list_integer(my_list=[]):
-    for item in my_list:
-        print("{:d}".format(item))
+    if my_list:
+        for item in my_list:
+            print("{:d}".format(item))
 
