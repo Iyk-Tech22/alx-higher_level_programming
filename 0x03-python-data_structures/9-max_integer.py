568933dbaaa99fda3ade2e5589c7f72ae043a67a
F. Godwin <ikechukwugodwin22@yahoo.com>
2022-10-02 12:58:29 +0100

    Update Debug | 😥

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return (None)
-    max = 0
-    for i in my_list:
-        if max < i:
-            max = i
-    return (max)
+    """
+    find the maximum value of a list
+    Args:
+        my_list - list to search
+    Return:
+        None - if list is empty
+        maximum of list
+    """
+    L = len(my_list)
+    if L == 0:
+        return None
+    Max = my_list[0]
+    for elem in my_list:
+        if elem > Max:
+            Max = elem
+    return Max
