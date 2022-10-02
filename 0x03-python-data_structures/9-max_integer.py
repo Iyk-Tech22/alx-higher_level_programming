413d3376fae8e2f8d0f2145fa7275c308ea7d02a
F. Godwin <ikechukwugodwin22@yahoo.com>
2022-10-02 12:52:15 +0100

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
-    else:
-        max = 0
-        for i in my_list:
-            if max < i:
-                max = i
-        return (max)
+    max = 0
+    for i in my_list:
+        if max < i:
+            max = i
+    return (max)
