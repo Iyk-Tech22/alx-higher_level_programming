94811b116e240e9e274396c1a578d7dfa1f5bec5
Ikechukwu F. Godwin <ikechukwugodwin22@yahoo.com>
2023-04-03 22:31:00 +0100

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# Sends a JSON POST request to a given URL with a given JSON file.
+#!/urs/bin/env bash
+# It sends a json post request to a given url with a given json file.
 curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
