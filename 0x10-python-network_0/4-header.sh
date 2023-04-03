5a619465f2a9899f35984ae6bf007398c4cd5c54
Ikechukwu F. Godwin <ikechukwugodwin22@yahoo.com>
2023-04-04 00:00:55 +0100

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
-#!/urs/bin/env bash
+#!/bin/bash
 # It takes in a url as an argument and makes a get request to the url and display the body of the response
 curl -sX GET $1 -H "X-School-User-Id: 98" -L
