14fd879a255490ed47087d40087223e03c2a46ed
Ikechukwu F. Godwin <ikechukwugodwin22@yahoo.com>
2023-04-03 22:28:51 +0100

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# Sends a GET request to a given URL and display the response status code.
+#!/use/bin/env bash
+# It sends a get request to a given url and display the response status code.
 curl -s -o /dev/null -w "%{http_code}" "$1"
