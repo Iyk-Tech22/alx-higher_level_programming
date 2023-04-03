268687f5482da3a4a1b8c8af0a91a5358f1c4981
Ikechukwu F. Godwin <ikechukwugodwin22@yahoo.com>
2023-04-03 22:39:48 +0100

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# Bash scripts that sends a POST request to a given URL.
+#!/use/bin/env bash
+# A bash script that makes a post request to a given URL.
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
