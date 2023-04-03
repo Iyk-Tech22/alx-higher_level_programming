281c16ead5b7a2f928b7824edb28b8c45e051463
Ikechukwu F. Godwin <ikechukwugodwin22@yahoo.com>
2023-04-03 22:32:54 +0100

    Update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# Send a DELETE request to a given URL and display the response body.
+#!/usr/bin/env bash
+# It send a delete request to a given url and display the response body
 curl -sX DELETE "$1"
